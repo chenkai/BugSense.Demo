90c2d72b5aaca3b07e8b84d117b71eebbe8c6b7c
chenkai <chenkaiHome@live.cn>
2012-02-07 15:15:24 +0800

    first Version BugSense

---
+++ BugsenseTrackLog_Demo/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+using BugSense;
+using BugSense.Notifications;
+namespace BugsenseTrackLog_Demo
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //throw new NullReferenceException("Test Case For BugSense Log Record Exception!");
+
+            try
+            {
+                throw new ArrayTypeMismatchException("Array Type Mismatch Error is raised", new Exception("Test Error!"));
+            }
+            catch (Exception ex)
+            {
+                //BugSenseHandler.HandleError(se, "This is my first commit Exception.", new NotificationOptions() { Text="first Notification", Title="error Title!" });
+                var overridenOptions = BugSenseHandler.DefaultOptions();
+                overridenOptions.Text = ex.Message + Environment.NewLine + "Do you want to send the Exception?";
+                overridenOptions.Type = enNotificationType.MessageBoxConfirm;
+                BugSenseHandler.HandleError(ex, string.Format("User {0}", "User define"), overridenOptions);
+            }
+        }
+    }
+}
